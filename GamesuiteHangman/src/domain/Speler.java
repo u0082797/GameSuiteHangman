a280600d76e95638a8d72f0422cc88e23d7dd6a9
DESKTOP-HQ1DK3E\Gebruiker <patrick.fox@ucll.be>
2017-05-07 14:37:24 +0200

    aaaa

---
+++ GamesuiteHangman/src/domain/Speler.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Speler {
+	private String naam;
+
+	public Speler (String naam){
+		this.naam = naam;
+	}
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+	
+	
+
+}
--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -7,8 +7,8 @@ import domain.Speler;
 public class Launcher {
 
 	public static void main(String[] args) {
-		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
-		Speler speler = new Speler(naam);
+		String naampiee = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
+		Speler speler = new Speler(naampiee);
 
 		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 	}
